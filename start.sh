faa25e1e05226535bc784344d8b02a677eb145a1
MoellerDi <MoellerDi@users.noreply.github.com>
2019-12-10 21:50:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 # set server timezone
 echo "$(date +%F_%R) [Note] Setting server timezone settings to '${TZ}'"
 echo "date.timezone = ${TZ}" >> /etc/php.ini
-
-# set custom php.ini enviorments to follow Cacti Recommendations requirment
-sed -i "s/^\(memory_limit =\).*/\1 ${PHP_MEMORY_LIMIT}/" /etc/php.ini
-sed -i "s/^\(max_execution_time =\).*/\1 ${PHP_MAX_EXECUTION_TIME}/" /etc/php.ini
-
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
+
+# set custom php.ini enviorments to follow Cacti Recommendations requirment
+echo "$(date +%F_%R) [Note] Applying php.ini enviromental variables"
+sed -i "s/^\(memory_limit =\).*/\1 ${PHP_MEMORY_LIMIT}/" /etc/php.ini
+sed -i "s/^\(max_execution_time =\).*/\1 ${PHP_MAX_EXECUTION_TIME}/" /etc/php.ini
 
 # verify if initial install steps are required, if lock file does not exist run the following   
 if [ ! -f /cacti/install.lock ]; then
